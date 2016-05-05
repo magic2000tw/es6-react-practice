b8c16dee8729393cc079d1384f73e07e0d312b55
magic2000tw <magic2000tw@gmail.com>
2016-05-05 17:41:54 +0800

    Update and rename BurgerMenu.js to BurgerIcon.js

--- BurgerIcon.js
+++ BurgerIcon.js
@@ -2,12 +2,9 @@ import React from 'react'
 import {render} from 'react-dom'
 import Radium from 'radium'
 
-class BurgerButton extends React.Component {
+class BurgerIcon extends React.Component {
   constructor(props){
     super(props)
-    this.state = {hover:false,isClick:false}
-    this.handleHover = this.handleHover.bind(this)
-    this.onClick = this.onClick.bind(this)
   }
 
   getIconStyle(index){
@@ -17,29 +14,22 @@ class BurgerButton extends React.Component {
       left: 0,
       right: 0,
       top: 20 * (index * 2) + '%',
-      opacity: this.state.hover ? 0.6 : 1,
+      opacity: this.props.hover ? 0.6 : 1,
       background: 'white',
       transition: 'all 0.5s'
     }
-    if(index == 0 && this.state.isClick){
+    if(index == 0 && this.props.isOpen){
       style.transform = 'rotate(45deg)';
       style.top = '50%';
     }
-    if(index == 1 && this.state.isClick){
+    if(index == 1 && this.props.isOpen){
       style.background = 'transparent';
     }
-    if(index == 2 && this.state.isClick){
+    if(index == 2 && this.props.isOpen){
       style.transform = 'rotate(-45deg)';
       style.top = '50%';
     }
     return style;
-  }
-
-  handleHover(){
-    this.setState({hover: !this.state.hover});
-  }
-  onClick(){
-    this.setState({isClick: !this.state.isClick});
   }
 
   render() {
@@ -57,17 +47,18 @@ class BurgerButton extends React.Component {
       width: '36px',
       height: '30px',
       left: '5%',
-      top: '26%',
+      top: '20%',
       zindex: 1
     };
     return(
-      <div onMouseEnter={this.handleHover}
-           onMouseLeave={this.handleHover}
-           onClick={this.onClick.bind(this)}
-           style={buttonStyle}>
+      <div style={buttonStyle}>
         {icon}
       </div>
     );
   }
 }
-module.exports = Radium(BurgerButton);
+BurgerIcon.propTypes = { hover: React.PropTypes.bool,
+                         isOpen: React.PropTypes.bool};
+BurgerIcon.defaultProps = { hover: false,
+                            isOpen: false };
+module.exports = Radium(BurgerIcon);
