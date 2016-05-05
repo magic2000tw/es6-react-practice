b2d86fdfb112fe65096adf20add211f78f082cf3
magic2000tw <magic2000tw@gmail.com>
2016-05-05 17:41:14 +0800

    Added files via upload

---
+++ MenuContent.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {render} from 'react-dom'
+import Radium from 'radium'
+
+class MenuContent extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  handleHover(){
+    this.setState({hover: !this.state.hover});
+  }
+
+  onClick(){
+    this.setState({isClick: !this.state.isClick});
+  }
+
+
+  render(){
+    const contentStyle = {
+      position: 'fixed',
+      width:'20%',
+      height:'90%',
+      background:'black',
+      top:'0%',
+      opacity: this.state.hover ? 0.6 : 1
+    };
+    return(
+      <div style={contentStyle}>
+      {this.props.menuContent}
+      </div>
+    )
+  }
+}
+
+module.exports = Radium(MenuContent);
