40d58fa03bde5063bde7c151a91e7d54054ce352
magic2000tw <magic2000tw@gmail.com>
2016-05-09 23:50:30 +0800

    trying dropdown menu

--- src/MenuContent.js
+++ src/MenuContent.js
@@ -5,29 +5,67 @@ import Radium from 'radium'
 class MenuContent extends React.Component{
   constructor(props){
     super(props)
-  }
-
-  handleHover(){
-    this.setState({hover: !this.state.hover});
+    this.state = {isClick: false}
+    this.onClick = this.onClick.bind(this)
+    this.dropDown = this.dropDown.bind(this)
   }
 
   onClick(){
     this.setState({isClick: !this.state.isClick});
   }
 
+  dropDown(isClick){
+    const dropDownStyle = {
+      display: isClick ?'block':'none',
+      color:'white',
+      minWidth: '160px',
+      padding: '12px 16px',
+      position: 'absolute',
+      transform: isClick ? 'height 200ms ease-in':'none',
+      transition: 'all 0.2s'
+    };
+    return dropDownStyle;
+  }
 
   render(){
     const contentStyle = {
       position: 'fixed',
       width:'300px',
-      height:'90%',
+      height:'92%',
       background:'black',
-      top:'0%',
-      opacity: this.state.hover ? 0.6 : 1
+      top:'0%'
+    };
+    const aStyle = {
+      color: 'white',
+      padding: '12px 16px',
+      textDecoration: 'none',
+      display: 'block'
+    };
+    const btnStyle = {
+      background:'black',
+      color:'white',
+      border:'none',
+      cursor:'pointer',
+      outline:'none',
+      width:'100%',
+      height:'15%',
+      fontSize:'1.5em',
+      textAlign:'left'
     };
     return(
       <div style={contentStyle}>
-      {this.props.children}
+      {/*this.props.children*/}
+        <button onClick={this.onClick}
+                  style={btnStyle}>
+        Title
+        </button>
+        <nav>
+          <div style={this.dropDown(this.state.isClick)}>
+            <a href={'#'} style={aStyle}>content1</a>
+            <a href={'#'} style={aStyle}>content2</a>
+            <a href={'#'} style={aStyle}>content3</a>
+          </div>
+        </nav>
       </div>
     )
   }
