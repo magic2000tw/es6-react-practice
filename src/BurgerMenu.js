090664461dd77b06ca5d5c9fccfe16b77b3fbd22
magic2000tw <magic2000tw@gmail.com>
2016-05-19 18:00:39 +0800

    Update BurgerMenu.js

--- src/BurgerMenu.js
+++ src/BurgerMenu.js
@@ -1,28 +1,56 @@
 import React from 'react'
 import {render} from 'react-dom'
 import Radium from 'radium'
+import MenuContainer from './MenuContainer.js'
+import MenuButton from  './MenuButton.js'
 
 class BurgerMenu extends React.Component{
   constructor(props){
     super(props)
-  }
-
-  handleHover(){
-    this.setState({hover: !this.state.hover});
+    this.state = {isOpen:false}
+    //this.onClick = this.onClick.bind(this)
   }
 
   onClick(){
-    this.setState({isClick: !this.state.isClick});
+    console.log('clicked!!');
+    this.setState({isOpen: !this.state.isOpen});
+  }
+
+  getMenu(){
+    const menuStyle = {
+      position:'absolute',
+      width:'60%',
+      height:'10%',
+      top:'10%',
+      left:'30%'
+    };
+    const contentStyle = {
+      position:'absolute',
+      width:'60%',
+      height:'20%',
+      top:'20%',
+      left:'20%',
+      color:'white'
+    };
+    return(
+      <div style={menuStyle}>
+        <i className={'fa fa-camera-retro fa-2x fa-inverse'}></i>
+        <span style={contentStyle}>MENU1</span>
+      </div>
+    );
   }
 
   render(){
     return(
-      <div>
-        <MenuContent>
+      <div className={'BurgerMenu'}>
+        <MenuContainer>
           {this.getMenu()}
-        </MenuContent>
-        <MenuButton>This is Menu</MenuButton>
+        </MenuContainer>
+        <MenuButton onClick={this.onClick.bind(this)}
+                    isOpen={this.state.isOpen}>{this.props.menuLable}</MenuButton>
       </div>
     );
   }
 }
+
+export default BurgerMenu;
