68c5364a0808fb06650fbfd582ce07cbaed40931
magic2000tw <magic2000tw@gmail.com>
2016-05-19 18:00:20 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,47 +1,16 @@
 import React from 'react'
 import {render} from 'react-dom'
 import Radium from 'radium'
-import MenuContainer from './MenuContainer.js'
-import MenuButton from './MenuButton.js'
+import BurgerMenu from './BurgerMenu.js'
 
 class Example extends React.Component{
-  // constructor(props){
-  //   super(props)
-  //   this.getMenu = this.get.bind(this)
-  // }
-
-  getMenu(){
-    const menuStyle = {
-      position:'absolute',
-      width:'60%',
-      height:'10%',
-      top:'10%',
-      left:'30%'
-    };
-    const contentStyle = {
-      position:'absolute',
-      width:'60%',
-      height:'20%',
-      top:'20%',
-      left:'20%',
-      color:'white'
-    };
-    return(
-      <div style={menuStyle}>
-        <i className={'fa fa-camera-retro fa-2x fa-inverse'}></i>
-        <span style={contentStyle}>MENU1</span>
-      </div>
-    );
-  }
 
   render(){
     return(
       <div>
-        <MenuContainer>
-          {this.getMenu()}
-        </MenuContainer>
+        <BurgerMenu menuLable={'This is menu'} />
         <main>
-          <MenuButton>This is Menu</MenuButton>
+          test1
         </main>
       </div>
     )
